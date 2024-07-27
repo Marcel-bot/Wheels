08c18dd11559fd44ab1e0c8a039c6c14c5e7b910
Karo8870 <cazacuchristian@gmail.com>
2024-07-27 21:51:55 +0300

    Added wheel control and I2C communication

---
+++ include/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN
+#define MAIN
+
+#include<Arduino.h>
+#include<Wire.h>
+
+#define I2C_ADDRESS 0x08
+
+#define LEFT_STEP_PIN 4
+#define LEFT_DIR_PIN  3
+#define LEFT_ENABLE_PIN 2
+
+#define RIGHT_STEP_PIN 7
+#define RIGHT_DIR_PIN  6
+#define RIGHT_ENABLE_PIN 5
+
+#define UP_MESSAGE "Wheels up"
+
+#define UP_CHAR '*'
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,101 @@
-#include <Arduino.h>
+#include<main.h>
 
-// put function declarations here:
-int myFunction(int, int);
+long leftMotorSpeed = 0;
+long rightMotorSpeed = 0;
+
+unsigned long lastStepTimeLeft = 0;
+unsigned long lastStepTimeRight = 0;
+
+bool leftMotorStatus = false;
+bool rightMotorStatus = false;
+
+bool received_confirmation = false;
+
+void stepMotor(int stepPin, unsigned long speed, unsigned long &lastStepTime, bool &status) {
+  if (speed) {
+    unsigned long currentTime = micros();
+
+    if (currentTime - lastStepTime >= speed) {
+      status = !status;
+
+      digitalWrite(stepPin, status);
+      lastStepTime = currentTime;
+    }
+  } else {
+    digitalWrite(stepPin, LOW);
+  }
+}
+
+void on_receive(const int size) {
+  if (size >= 4) {
+    byte highByte = Wire.read();
+    byte lowByte = Wire.read();
+
+    leftMotorSpeed = (highByte << 8) | lowByte;
+    digitalWrite(LEFT_DIR_PIN, leftMotorSpeed >= 0);
+    leftMotorSpeed = abs(leftMotorSpeed);
+
+    highByte = Wire.read();
+    lowByte = Wire.read();
+
+    rightMotorSpeed = (highByte << 8) | lowByte;
+    digitalWrite(RIGHT_DIR_PIN, rightMotorSpeed >= 0);
+    rightMotorSpeed = abs(rightMotorSpeed);
+  }
+}
+
+void no_request() {}
+
+void send_up() {
+    if (received_confirmation) {
+        Wire.write(UP_MESSAGE);
+
+        Wire.onRequest(no_request);
+        Wire.onReceive(on_receive);
+    }
+}
+
+void await_confirmation(const int size) {
+    while (Wire.available()) {        
+        if (Wire.read() == UP_CHAR) {
+            received_confirmation = true;
+        }
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LEFT_STEP_PIN, OUTPUT);
+  pinMode(LEFT_DIR_PIN, OUTPUT);
+  pinMode(LEFT_ENABLE_PIN, OUTPUT);
+  pinMode(RIGHT_STEP_PIN, OUTPUT);
+  pinMode(RIGHT_DIR_PIN, OUTPUT);
+  pinMode(RIGHT_ENABLE_PIN, OUTPUT);
+
+  digitalWrite(LEFT_ENABLE_PIN, LOW);
+  digitalWrite(RIGHT_ENABLE_PIN, LOW);
+
+  digitalWrite(LEFT_DIR_PIN, HIGH);
+  digitalWrite(RIGHT_DIR_PIN, HIGH);
+
+  Wire.begin(I2C_ADDRESS);
+
+  Wire.onRequest(send_up);
+  Wire.onReceive(await_confirmation);
+
+  while (!received_confirmation) {
+      delay(10);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (Serial.available() >= 2 * sizeof(int)) {
+    leftMotorSpeed = Serial.parseInt();
+    rightMotorSpeed = Serial.parseInt();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    digitalWrite(LEFT_DIR_PIN, leftMotorSpeed > 0);
+    digitalWrite(RIGHT_DIR_PIN, rightMotorSpeed > 0);
+  }
+
+  stepMotor(LEFT_STEP_PIN, leftMotorSpeed, lastStepTimeLeft, leftMotorStatus);
+  stepMotor(RIGHT_STEP_PIN, rightMotorSpeed, lastStepTimeRight, rightMotorStatus);
 }
